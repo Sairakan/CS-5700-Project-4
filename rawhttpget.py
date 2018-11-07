4bd9f6ae8af7af543f81008b0d5a573fcdea3366
jteng <akariass@gmail.com>
2018-11-07 09:36:33 -0800

    get hostIP

--- rawhttpget.py
+++ rawhttpget.py
@@ -2,7 +2,16 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+Authors: Jason Teng, Jae Son
 """
 
-from socket import socket, AF_PACKET, SOCK_RAW
+from socket import AF_PACKET, SOCK_RAW, IPPROTO_RAW, IPPROTO_IP
+import socket
+
+sendSock = socket.socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW)
+recSock = socket.socket(AF_PACKET, SOCK_RAW, IPPROTO_IP)
+
+# gets the host ip by creating a connection to Google and observing the socket parameters
+hostIP = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+
+print hostIP
