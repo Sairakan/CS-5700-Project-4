2c6ebb02a2ec41044c118e61e49142daf9ed4995
jteng <akariass@gmail.com>
2018-11-10 09:01:28 -0800

    added basic stubs for ipunwrap

--- rawhttpget.py
+++ rawhttpget.py
@@ -23,9 +23,26 @@ hostIP = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [
 
 print(hostIP)
 
-for i in range(10):
-    packet = recSock.recv(65565)
-    print(packet)
+ip_header_format = '!BBHHHBBH4s4s'
+tcp_header_format = '!HHLLBBHHH'
+
+# ip header fields
+ip_ihl = 5
+ip_ver = 4
+ip_tos = 0
+ip_tot_len = 0  # kernel will fill the correct total length
+ip_id = 54321   # Id of this packet
+ip_frag_off = 0
+ip_ttl = 255
+ip_proto = socket.IPPROTO_TCP
+ip_check = 0    # kernel will fill the correct checksum
+ip_saddr = socket.inet_aton(hostIP)
+ip_daddr = socket.inet_aton('8.8.8.8')
+
+ip_ihl_ver = (ip_ver << 4) + ip_ihl
+
+# the ! in the pack format string means network order
+ip_header = struct.pack('!BBHHHBBH4s4s' , ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
 
 """
 Takes in TCP header parameters and creates the correct TCP header and adds it to the data.
@@ -45,7 +62,26 @@ data: the data to be wrapped
 RETURNS:
 the packet wrapped with the TCP header
 """
-def TCPWrapper(src, dest, seq, ack, offset, flags, awnd, chksm, urg, opt, data):
-    tcp_header = struct.pack('!HHLLBBHHH', src, dest, seq, ack, offset, flags, awnd, chksm, urg)
+def tcpwrapper(src, dest, seq, ack, offset, flags, awnd, chksm, urg, opt, data):
+    tcp_header = struct.pack(tcp_header_format, src, dest, seq, ack, offset, flags, awnd, chksm, urg)
     tcp_packet = tcp_header + opt + data
     return tcp_packet
+
+def tcpunwrap(tcp_packet):
+    tcp_header = struct.unpack(tcp_header_format, tcp_packet[0:20])
+    tcp_data = tcp_packet[struct.calcsize(tcp_header_format):]
+    return tcp_data
+
+def ipunwrap(ip_packet):
+    ip_header = struct.unpack(ip_header_format, ip_packet[0:20])
+    # verify the ip header is correct
+    # get the data from the ip packet
+    ip_data = ip_packet[struct.calcsize(ip_header_format):]
+    return ip_data
+
+
+for i in range(2):
+    packet = recSock.recv(65565)
+    data = tcpunwrap(ipunwrap(packet))
+    print(data)
+    print(data.decode() + '\n')
