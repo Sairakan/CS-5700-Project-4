2e095715955bd510fed337fb0df1e59229acc8bc
Jay Son <sjson421@gmail.com>
2018-11-15 05:15:33 -0800

    send progress and cleanup. Needs handshake

--- rawhttpget.py
+++ rawhttpget.py
@@ -20,7 +20,6 @@ recSock.settimeout(180000)
 
 # gets the host ip by creating a connection to Google and observing the socket parameters
 hostIP = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
-
 print(hostIP)
 
 hostIP_hex = bytes(map(int, hostIP.split('.')))
@@ -50,6 +49,16 @@ tcp_header_format = '!HHLLBBHHH'
 tcp_header_keys = ['src', 'dest', 'seq', 'ack', 'off_res', 'flags', 'awnd', 'chksm', 'urg']
 pseudo_header_format = '4s4sBBH'
 
+# URL trimming to get host name for DEST_ADDR
+trimUrl = url
+if url.startswith('http://'):
+    trimUrl = url[7:]
+elif url.startswith('https://'):
+    trimUrl = url[8:]
+if '/' in trimUrl:
+    i = trimUrl.find('/')
+    trimUrl = trimUrl[0:i]
+        
 # These should be constant for the whole program, while sending packets
 # TCP Side
 SRC_PORT = random.randint(1024, 65530)
@@ -67,12 +76,12 @@ IP_HDR_LEN = 20
 TTL = 255
 PROTO = socket.IPPROTO_TCP
 SRC_ADDR = socket.inet_aton(hostIP)
+DEST_ADDR = socket.inet_aton(socket.gethostbyname(trimUrl))
 
-def tcpwrap(destAddr, seq, ack, flags, opt, data):
+def tcpwrap(seq, ack, flags, opt, data):
     """
     Takes in TCP header parameters and creates the correct TCP header and adds it to the data.
     Returns the new message with the TCP header added. Offset is automatically calculated.
-    :param destAddr: destination address
     :param seq: the sequence number of the current packet. += 1 beforehand.
     :param ack: the acknowledgment number
     :param flags: any flags
@@ -86,7 +95,7 @@ def tcpwrap(destAddr, seq, ack, flags, opt, data):
     temp_header = pack(tcp_header_format, SRC_PORT, DEST_PORT, seq, ack, offset << 4, 
                       flags,  AWND, URG)
     total_len = len(temp_header) + len(data)
-    pseudo_header = pack(pseudo_header_format , SRC_ADDR , destAddr, 0, PROTO, total_len);
+    pseudo_header = pack(pseudo_header_format , SRC_ADDR , DEST_ADDR, 0, PROTO, total_len);
     check = checksum(temp_header + pseudo_header + data)
     
     tcp_header = pack(tcp_header_format , SRC_PORT, DEST_PORT, seq, ack, offset, flags,  
@@ -118,10 +127,9 @@ def tcpunwrap(tcp_packet):
         #TCP HEADER OR DATA CHECKSUM HAS FAILED. TODO
         print ('checksum has failed. replicate TCP ACK behavior')
 
-def ipwrap(dest_addr, tcp_packet):
+def ipwrap(tcp_packet):
     """
     Takes in the IP header parameters and constructs a IP header, which is added to the given data and returned.
-    :param dest_addr: the destination IP address
     :param tcp_packet: the full packet given out by tcpwrap, including payload
     :return: the full IP packet, including the TCP packet
     """
@@ -129,7 +137,7 @@ def ipwrap(dest_addr, tcp_packet):
     pktId = random.randint(0, 65534)
     total_len = len(tcp_packet) + 20
     return pack(ip_header_format, IHL_VERSION, TOS, total_len, pktId, FRAG_OFF, 
-                TTL, PROTO, check, SRC_ADDR, dest_addr) + tcp_packet
+                TTL, PROTO, check, SRC_ADDR, DEST_ADDR) + tcp_packet
 
 def ipunwrap(ip_packet):
     ip_header_vals = unpack(ip_header_format, ip_packet[0:20])
@@ -197,62 +205,31 @@ def ip_verify_checksum(headerVals):
     return (calculatedChecksum == checksum)
     
 # VERY untested. To be completed later.
-# Re-implement using sendPacket function
+# TODO: finish function and test
 def tcp_handshake():
-    '''
+    seq = random.randint(0,1000000)
+    #tcp flags
+    tcp_fin = 0
+    tcp_syn = 1
+    tcp_rst = 0
+    tcp_psh = 0
+    tcp_ack = 0
+    tcp_urg = 0
+    tcp_flags = tcp_fin + (tcp_syn << 1) + (tcp_rst << 2) + (tcp_psh <<3) + (tcp_ack << 4) + (tcp_urg << 5)
     
-    tcp_seq = 1
-    tcp_ack_seq = 0
-    
-    ip_header = ipwrap(ip_ver, ip_ihl, ip_tos, ip_tot_len, ip_id, ip_frag_off, 
-                       ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
-    tcp_header = pack('!HHLLBBHHH' , tcp_source, tcp_dest, tcp_seq, 
-                        tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window, tcp_check, 
-                        tcp_urg_ptr) 
-    
-    dest_ip = socket.gethostbyname(url)
-    source_address = socket.inet_aton( hostIP )
-    dest_address = socket.inet_aton(dest_ip)
-    placeholder = 0
-    protocol = socket.IPPROTO_TCP
-    tcp_length = len(tcp_header)
-    
-    psh = struct.pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
-    psh = psh + tcp_header;
-    
-    new_check = checksum(psh)
-    tcp_header = tcpwrap('!HHLLBBH' , tcp_source, tcp_dest, tcp_seq, 
-                        tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window, 
-                        struct.pack('H' , new_check),
-                        struct.pack('!H' , tcp_urg_ptr))
-    
-    # final full packet - syn packets dont have any data
-    packet = ip_header + tcp_header
-    s.sendto(packet, (dest_ip , 0 ))
+    #sendPacket(seq, 0, tcp_flags, <SOME OPTIONS>, '')
     
     received = s.recvfrom(1024)
     received_ack = received[3]
     
-    if tcp_seq == received_ack - 1:
-        tcp_seq = 2
-    
-        tcp_header = tcpwrap('!HHLLBBH' , tcp_source, tcp_dest, tcp_seq, 
-                            tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window, 
-                            struct.pack('H' , new_check),
-                            struct.pack('!H' , tcp_urg_ptr))
-        packet = ip_header + tcp_header
-        s.sendto(packet, (dest_ip, 0))
+    if seq == received_ack - 1:
+        #TODO
+        print("Got an ACK back. Time to send an ACK and finish handshake.")
     else:
         print("Handshake failed!")
-    '''
         
 def change_file_name(myUrl):
     fileName = ''
-    if myUrl.startswith('http://'):
-        myUrl = myUrl[7:]
-    elif myUrl.startswith('https://'):
-        myUrl = myUrl[8:]
-        
     lastSlashIndex = myUrl.rfind('/')
     if lastSlashIndex == -1:
         fileName = 'index.html'
@@ -263,72 +240,11 @@ def change_file_name(myUrl):
             fileName = myUrl[lastSlashIndex + 1:]
     return fileName
 
-# TODO!!!!!! THIS SHOULD BE THE FIRST THING TO DO NEXT TIME!
-def sendPacket():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-        s.bind(("", "63798"))
-    except socket.error:
-        print ('Socket creation failed!')
-        exit(0)
-    
-    packet = ''; 
-    source_ip = '172.16.87.84'
-    dest_ip = '172.16.10.1'
-    
-    # ip header fields
-    ip_ihl = 5
-    ip_ver = 4
-    ip_tos = 0
-    ip_tot_len = 0
-    ip_id = 54321
-    ip_frag_off = 0
-    ip_ttl = 255
-    ip_proto = socket.IPPROTO_TCP
-    ip_check = 0
-    ip_saddr = socket.inet_aton ( source_ip )
-    ip_daddr = socket.inet_aton ( dest_ip ) 
-    ip_ihl_ver = (ip_ver << 4) + ip_ihl
-    
-    ip_header = pack('!BBHHHBBH4s4s' , ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
-    
-    # tcp header fields
-    tcp_source = 63798   # source port
-    tcp_dest = 8888   # destination port
-    tcp_seq = 104
-    tcp_ack_seq = 0
-    tcp_doff = 5    #4 bit size of tcp header, 5 * 4 = 20 bytes
-    #tcp flags
-    tcp_fin = 0
-    tcp_syn = 1
-    tcp_rst = 0
-    tcp_psh = 0
-    tcp_ack = 0
-    tcp_urg = 0
-    tcp_window = socket.htons (5840)
-    tcp_check = 0
-    tcp_urg_ptr = 0
-    
-    tcp_offset_res = (tcp_doff << 4) + 0
-    tcp_flags = tcp_fin + (tcp_syn << 1) + (tcp_rst << 2) + (tcp_psh <<3) + (tcp_ack << 4) + (tcp_urg << 5)
-    
-    tcp_header = pack('!HHLLBBHHH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window, tcp_check, tcp_urg_ptr) 
-    
-    source_address = socket.inet_aton( source_ip )
-    dest_address = socket.inet_aton(dest_ip)
-    placeholder = 0
-    protocol = socket.IPPROTO_TCP
-    tcp_length = len(tcp_header)
-    
-    psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
-    psh = psh + tcp_header;
-    
-    tcp_check = checksum(psh)
-    
-    tcp_header = pack('!HHLLBBH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window) + pack('H' , tcp_check) + pack('!H' , tcp_urg_ptr)
-    
-    packet = ip_header + tcp_header
-    s.sendto(packet, (dest_ip , 8888 ))
+# Use this without messing with IP wrap and TCP wrap, ideally
+def sendPacket(seq, ack, flags, opt, data):
+    tcpPacket = tcpwrap(seq, ack, flags, opt, data)
+    ipPacket = ipwrap(tcpPacket)
+    s.sendto(ipPacket, (DEST_ADDR , DEST_PORT))
 #############################################################################
 
 def run():
